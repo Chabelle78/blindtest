3fa8587ea4d9b767d02a534c5c3acdb31186d3df
JIdayyy <julien_abbadie@hotmail.fr>
2021-05-11 13:04:04 +0200

    game component template ok

--- hackhathon/src/components/Main/Main.jsx
+++ hackhathon/src/components/Main/Main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Game from "../Game/Game";
 import Scores from "../Scores/Scores";
 import Upload from "../Upload/Upload";
-import './Main.css'
+import "./Main.css";
 
 export default function Main() {
   return (
     <div className="main flex items-center align-middle justify-center w-screen h-screen">
-      <p>Hello world</p>
       <Scores />
       <Game />
       <Upload />
