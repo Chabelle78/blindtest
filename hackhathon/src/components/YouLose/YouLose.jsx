1ee463af03e28a0bf90a87f2fcfd8792e5808677
rcarondw <r.carondw@gmail.com>
2021-05-11 16:05:00 +0200

    reborn Youlose

--- hackhathon/src/components/YouLose/YouLose.jsx
+++ hackhathon/src/components/YouLose/YouLose.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function YouLose(){
     return (
         <div>
-            <h1>So sade; you lose... !!</h1>
+            <h1>So sad, you lose... !!</h1>
             {/* <img src="" alt="" />  data.picture + data.name*/}
             {/* <p></p>  data.artiste data.track_name*/}
             <button>Try Again !! </button>
