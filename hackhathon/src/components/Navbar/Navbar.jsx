c51cd71c72e3b74c8ca869992097e42d99f5144e
JIdayyy <julien_abbadie@hotmail.fr>
2021-05-11 19:50:53 +0200

    merge ok

--- hackhathon/src/components/Navbar/Navbar.jsx
+++ hackhathon/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { LogoutIcon } from '@heroicons/react/solid';
-
+import { LogoutIcon } from "@heroicons/react/solid";
 
 export default function Navbar() {
   return (
-<<<<<<< HEAD
-    <div className="w-full h-16 fixed top-0 bg-gray-900">
-      <p>Navbar</p>
+    <div className="w-full h-16 bg-gray-900">
+      <div className="flex justify-end items-center h-full">
+        <p className="mr-5 text-xl text-white flex items-center">UserName</p>
+        <LogoutIcon className="text-white h-6 mr-5" />
+      </div>
     </div>
-=======
-  <div className="w-full h-16 bg-gray-900">
-    <div className="flex justify-end items-center h-full">
-      <p className="mr-5 text-xl text-white flex items-center">UserName</p>
-      <LogoutIcon className="text-white h-6 mr-5"/>
-    </div>
-  </div>
->>>>>>> dev
   );
 }
