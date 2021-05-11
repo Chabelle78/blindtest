0d8ba4a6eeab107af924c86256c4b656956111d7
rcarondw <r.carondw@gmail.com>
2021-05-11 19:23:39 +0200

    added button for quit PopUp

--- hackhathon/src/components/PopUp/PopUp.jsx
+++ hackhathon/src/components/PopUp/PopUp.jsx
@@ -2,22 +2,13 @@ import React from 'react';
 
 export default function PopUp(){
 
-    //if(win){
     return (
-        <div className="w-full h-screen bg-black opacity-60">
+        <div className="w-full h-screen bg-black bg-opacity-60 flex-col justify-center items-center">
             <div className="h-1/4 w-1/4 flex justify-center items-center">
                 <p className="flex justify-center items-center">You Won !!</p>
             </div>
+            <button>Ok !</button>
         </div>
     )
-    //} else {
 
-    return (
-        <div className="w-full h-screen bg-black opacity-60">
-        <div className="h-1/4 w-1/4 flex justify-center items-center">
-            <p className="flex justify-center items-center">You Lose... !!</p>
-        </div>
-    </div>
-    )
-//  }
 }
