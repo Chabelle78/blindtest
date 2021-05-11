ffbc79ba075d4ef149bb20ce2fe58e68f6feda9c
rcarondw <r.carondw@gmail.com>
2021-05-11 12:28:59 +0200

    last refresh befor pr

--- hackhathon/src/App.jsx
+++ hackhathon/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="flex flex-col">
-      {/* <Navbar /> */}
+      <Navbar />
       <Main />
     </div>
   );
