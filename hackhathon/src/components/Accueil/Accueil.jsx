ded290f5cf190e7f713bdb6fcd01e478e0dc11b4
rcarondw <r.carondw@gmail.com>
2021-05-11 20:21:15 +0200

    remove accueil from score to component

--- hackhathon/src/components/Accueil/Accueil.jsx
+++ hackhathon/src/components/Accueil/Accueil.jsx
@@ -4,7 +4,10 @@ export default function Accueil(){
 
     return (
         <div>
-            
+            <h2>Super Blind Test</h2>
+            <button className="h-16 w-16 rounded-full bg-purple-800 text-white">
+                Play
+            </button>
         </div>
     )
 }
