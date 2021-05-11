4abaa156b5fa27d8b249351ea3dc558499990d35
JIdayyy <julien_abbadie@hotmail.fr>
2021-05-11 13:46:12 +0200

    template score

--- hackhathon/src/components/Scores/Scores.jsx
+++ hackhathon/src/components/Scores/Scores.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Scores() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center align-middle justify-center">
+      <div className="w-80 h-80 bg-black opacity-50"></div>
+    </div>
+  );
 }
