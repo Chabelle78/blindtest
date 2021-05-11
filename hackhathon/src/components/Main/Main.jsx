b1ac654b1a9d8c1f663d9946f681fc5992ac56f9
rcarondw <r.carondw@gmail.com>
2021-05-11 12:27:53 +0200

    refresh before pr

--- hackhathon/src/components/Main/Main.jsx
+++ hackhathon/src/components/Main/Main.jsx
@@ -8,9 +8,9 @@ export default function Main() {
   return (
     <div className="main flex items-center align-middle justify-center w-screen h-screen">
       <p>Hello world</p>
-      {/* <Scores /> */}
-      {/* <Game />
-      <Upload /> */}
+      <Scores />
+      <Game />
+      <Upload />
     </div>
   );
 }
