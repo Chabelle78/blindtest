074fcbdcb3f40ae9341b7ff3056ff96623442557
rcarondw <r.carondw@gmail.com>
2021-05-11 15:48:13 +0200

    YouWon components structur done

---
+++ hackhathon/src/components/YouWon/YouWon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function YouWon(){
+    return (
+        <div>
+            <h1>Well done, you Won !!</h1>
+            {/* <img src="" alt="" />  data.picture + data.name*/}
+            {/* <p></p>  data.artiste data.track_name*/}
+            <button>Try Again !! </button>
+        </div>
+    )
+}
